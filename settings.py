d3cb4e7d2b72b2ef9c793fae8aac80f3d4bf8bc7
Nishant Ranjan <darkfist1998@gmail.com>
2016-09-19 22:32:43 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings():
     def __init__(self):
         # screen settings
         self.screen_width = 1000
-        self.screen_height = 700
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
 
         # ship settings
